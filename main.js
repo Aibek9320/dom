7ebc0326002708e059fe3e78afb41af6d6bf61c4
Aiba <saiba9320@mail.ru>
2023-02-23 21:00:50 +0600

    first

---
+++ main.js
@@ -0,0 +1,14 @@
+// const task1 = (list) => {
+//     return document.querySelector('li').textContent
+// }
+// console.log(task1())
+
+// const task2 = () => {
+//     return Array.from(document.getElementsByTagName('li')).map(el => el.textContent)
+// }
+// console.log(task2())
+
+const task3 = () => {
+    return
+}
+console.log(task3())
