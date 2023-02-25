5f20258139981213c9b29a70b07041c6c2b420d0
Aibek9320 <123246995+Aibek9320@users.noreply.github.com>
2023-02-25 17:30:43 +0600

    task6

--- main.js
+++ main.js
@@ -8,7 +8,22 @@
 // }
 // console.log(task2())
 
-const task3 = () => {
-    return
+// const task3 = () => {
+//     return Array.from(document.getElementsByClassName('list-item')).map(el => el.textContent)
+// }
+// console.log(task3())
+
+// const task4 = () => {
+//     return Array.from(document.getElementsByClassName('list-item')).map(elem =>elem.getAttribute('class'))
+// }
+// console.log(task4())'
+
+// const task5 = () => {
+//     return Array.from(document.getElementsByClassName('list-item')).map(elem =>elem.getAttribute('data-test-id'))
+// }
+// console.log(task5())
+
+const task6 = () => {
+    return Array.from(document.getElementById('data-test-id'))
 }
-console.log(task3())
+console.log(task6())
