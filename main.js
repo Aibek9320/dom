be7a85f1135f2a0a9a8666c2f859876a8290ebc1
Aiba <saiba9320@mail.ru>
2023-02-26 12:46:48 +0600

    9

--- main.js
+++ main.js
@@ -23,7 +23,25 @@
 // }
 // console.log(task5())
 
-const task6 = () => {
-    return Array.from(document.getElementById('data-test-id'))
-}
-console.log(task6())
+// const task6 = () => {
+//     return Array.from(document.getElementsByClassName('list-item'))
+//     .filter((el) => !el.className.includes(el.textContent))
+//     .map((el) => el.getAttribute('data-test-id'))
+// }
+// console.log(task6())
+
+// const task7 = () => {
+//     return Array.from(document.getElementsByClassName('list-item'))
+//     .forEach(el => el.textContent=`${el.getAttribute('data-test-id')} ${el.textContent}`)
+// }
+// task7()
+
+// const task8 = () => {
+//     return Array.from(document.getElementsByClassName('list-item'))
+//          .filter((el) => !el.className.includes(el.textContent))
+//          .forEach((el) => el.parentNode.removeChild(el))
+// }
+// console.log(task8())
+
+// task9 
+console.log(document.querySelectorAll('#secret-password')[0].placeholder = 'you was hacked');
